caadb219642684aad831787cc2b557a29675a765
Nicholas Kovalchuk <dworak.niko@gmail.com>
2025-07-28 20:37:37 +0300

    changed 6 line text information

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println("Valid solution");
+        System.out.println("Hello world!");
 
     }
 }
